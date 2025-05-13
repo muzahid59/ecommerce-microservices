759d8f4b6d3a9526d06585336e7aef3756652237
Muzahidul Islam <mmsddam@gmail.com>
2025-05-13 09:10:49 +0600

    email service initiated

---
+++ services/email/src/controllers/index.ts
@@ -0,0 +1,3 @@
+export { default as verifyAccessToken } from './verifyAccessToken';
+export { default as userRegistration } from './userRegistration';
+export { default as userLogin } from './userLogin';
